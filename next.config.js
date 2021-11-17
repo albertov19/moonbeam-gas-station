2972d1d234c78f5831be481c211dbe325f81621a
Alberto Viera <albertoviera@gmail.com>
2021-11-17 17:27:55 +0100

    fixed nextjs images issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-const withImages = require("next-images");
-module.exports = {
-  ...withImages(),
-  ...withImages(),
+// next.config.js
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
+  images: {
+    loader: "imgix",
+    path: "",
+  },
   future: {
     webpack5: true,
   },
-};
+});
