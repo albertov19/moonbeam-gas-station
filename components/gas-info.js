d20a7837bb9357e6e059bbf4f607eabc8d64df25
Albertov19 <albertoviera@gmail.com>
2022-08-15 16:41:05 +0200

    added moonbeam-dev node

--- components/gas-info.js
+++ components/gas-info.js
@@ -1,75 +1,84 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "semantic-ui-react";
+
 import * as ethers from "ethers";
 
-class GasInfo extends Component {
-  static async getInitialProps() {
-    onUpdate();
-  }
-
+const GasInfo = ({ network }) => {
   // Initial State
-  state = {
-    currentBlock: 0,
-    avgGasPrice: 0,
-    minGasPrice: 0,
-    maxGasPrice: 0,
-  };
+  const [connected, setConnected] = useState(false);
+  const [currentBlock, setCurrentBlock] = useState("0");
+  const [nTxs, setNTxs] = useState("0");
+  const [avgGasPrice, setAvgGasPrice] = useState("0");
+  const [minGasPrice, setMinGasPrice] = useState("0");
+  const [maxGasPrice, setMaxGasPrice] = useState("0");
 
-  async componentDidMount() {
-    await this.onUpdate();
-  }
+  useEffect(async () => {
+    setInterval(async () => await onUpdate(), 5000);
+  }, []);
 
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  onUpdate = async () => {
+  const onUpdate = async () => {
+    // Get Web3 Provider
+    let web3;
     try {
-      // Get Web3 Provider
-      let web3;
-      if (this.props.network === "moonbeam") {
-        web3 = new ethers.providers.StaticJsonRpcProvider("https://rpc.api.moonbeam.network", {
-          chainId: 1284,
-          name: "moonbeam",
-        });
-      } else if (this.props.network === "moonriver") {
-        web3 = new ethers.providers.StaticJsonRpcProvider(
-          "https://rpc.api.moonriver.moonbeam.network",
-          {
-            chainId: 1285,
-            name: "moonriver",
-          }
-        );
-      } else if (this.props.network === "moonbase") {
-        web3 = new ethers.providers.StaticJsonRpcProvider(
-          "https://rpc.api.moonbase.moonbeam.network",
-          {
-            chainId: 1287,
-            name: "moonbase-alpha",
-          }
-        );
-      } else {
-        console.log("Network not supported");
+      switch (network) {
+        case "moonbeam":
+          web3 = new ethers.providers.StaticJsonRpcProvider("https://rpc.api.moonbeam.network", {
+            chainId: 1284,
+            name: "moonbeam",
+          });
+          break;
+        case "moonriver":
+          web3 = new ethers.providers.StaticJsonRpcProvider(
+            "https://rpc.api.moonriver.moonbeam.network",
+            {
+              chainId: 1285,
+              name: "moonriver",
+            }
+          );
+          break;
+        case "moonbase":
+          web3 = new ethers.providers.StaticJsonRpcProvider(
+            "https://rpc.api.moonbase.moonbeam.network",
+            {
+              chainId: 1287,
+              name: "moonbase",
+            }
+          );
+          break;
+        case "moonbeam-dev":
+          web3 = new ethers.providers.StaticJsonRpcProvider("http://127.0.0.1:9933", {
+            chainId: 1281,
+            name: "moonbeam-dev",
+          });
+          break;
+        default:
+          console.error("Network not supported");
+          break;
       }
 
       // Fetch Latest Block
       const latestBlock = await web3.getBlockNumber();
+
+      setConnected(true);
 
       // Fetch Latest Block Information
       const blockTxInfo = (await web3.getBlockWithTransactions(latestBlock)).transactions;
 
       // Loop through tx to get the gas price of each
       let gasPrices = Array();
-      for (let i = 0; i < blockTxInfo.length - 1; i++) {
+      for (let i = 0; i < blockTxInfo.length; i++) {
         gasPrices[i] = blockTxInfo[i].gasPrice / ethers.BigNumber.from("1000000000");
       }
       // Get Avg Gas Price, Min Gas Price and Max Gas Price
+      let nTxs;
       let avgGasPrice;
       let minGasPrice;
       let maxGasPrice;
       // Check if there are txs in the block
       if (gasPrices.length) {
+        nTxs = gasPrices.length;
         // If so, check if there is more than one tx and handle each case
-        if (gasPrices.length === 0) {
+        if (gasPrices.length === 1) {
           avgGasPrice = gasPrices[0];
         } else {
           avgGasPrice = gasPrices.reduce((a, b) => a + b) / gasPrices.length;
@@ -79,53 +88,62 @@ class GasInfo extends Component {
       } else {
         // If there were no txs, the min gas Price is still valid
         let gasPrice = (await web3.getGasPrice()) / ethers.BigNumber.from("1000000000");
+        nTxs = 0;
         avgGasPrice = gasPrice;
         minGasPrice = gasPrice;
         maxGasPrice = gasPrice;
       }
 
       // Save variables
-      this.setState({
-        currentBlock: latestBlock,
-        avgGasPrice: avgGasPrice.toFixed(1),
-        minGasPrice: minGasPrice.toFixed(1),
-        maxGasPrice: maxGasPrice.toFixed(1),
-      });
+      setCurrentBlock(latestBlock.toString());
+      setNTxs(nTxs.toString());
+      setAvgGasPrice(avgGasPrice.toFixed(1));
+      setMinGasPrice(minGasPrice.toFixed(1));
+      setMaxGasPrice(maxGasPrice.toFixed(1));
     } catch (error) {
-      console.log(error);
+      setConnected(false);
     }
-    this.intervalID = setTimeout(this.onUpdate.bind(this), 5000);
   };
 
-  render() {
-    return (
-      <div>
-        <h3>Gas Price from Latest Block {this.state.currentBlock}</h3>
-        <table className="ui celled table">
-          <tbody>
-            <tr>
-              <td style={{ width: "60%" }}>
-                <b>Min. Gas Price of Block</b>
-              </td>
-              <td style={{ textAlign: "right" }}>{this.state.minGasPrice} GWei</td>
-            </tr>
-            <tr>
-              <td style={{ width: "60%" }}>
-                <b>Max. Gas Price of Block</b>
-              </td>
-              <td style={{ textAlign: "right" }}>{this.state.maxGasPrice} GWei</td>
-            </tr>
-            <tr>
-              <td style={{ width: "60%" }}>
-                <b>Avg. Gas Price of Block</b>
-              </td>
-              <td style={{ textAlign: "right" }}>{this.state.avgGasPrice} GWei</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+  return (
+    <Container>
+      {connected ? (
+        <div>
+          <h3>Gas Price from Latest Block {currentBlock}</h3>
+          <table className="ui celled table">
+            <tbody>
+              <tr>
+                <td style={{ width: "60%" }}>
+                  <b>Number of Txs</b>
+                </td>
+                <td style={{ textAlign: "right" }}>{nTxs}</td>
+              </tr>
+              <tr>
+                <td style={{ width: "60%" }}>
+                  <b>Min. Gas Price of Block</b>
+                </td>
+                <td style={{ textAlign: "right" }}>{minGasPrice} GWei</td>
+              </tr>
+              <tr>
+                <td style={{ width: "60%" }}>
+                  <b>Max. Gas Price of Block</b>
+                </td>
+                <td style={{ textAlign: "right" }}>{maxGasPrice} GWei</td>
+              </tr>
+              <tr>
+                <td style={{ width: "60%" }}>
+                  <b>Avg. Gas Price of Block</b>
+                </td>
+                <td style={{ textAlign: "right" }}>{avgGasPrice} GWei</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h4>Network not Available</h4>
+      )}
+    </Container>
+  );
+};
 
 export default GasInfo;
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,54 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container, Grid } from "semantic-ui-react";
 import Head from "next/head";
 import Image from "next/image";
 
 import GasInfo from "../components/gas-info";
 
-class MoonbeamGasStation extends Component {
-  async componentDidMount() {}
-  render() {
-    return (
-      <Container>
-        <br />
-        <Head>
-          <title>Moonbeam Gas Station</title>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-          <link
-            rel="stylesheet"
-            href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-          />
-        </Head>
-        <Grid>
-          <Grid.Column width={8} stretched verticalAlign="top">
-            <Image src={require("/public/moongas.gif")} width={8} height={1} layout="responsive" />
-          </Grid.Column>
-        </Grid>
-        <h1>Moonbeam Network Basic Gas Station</h1>
-        <Grid>
-          <Grid.Column width={8} stretched verticalAlign="top">
-            <h2>Moonbeam</h2>
-            <GasInfo network={"moonbeam"} />
-          </Grid.Column>
-        </Grid>
-        <Grid>
-          <Grid.Column width={8} stretched verticalAlign="top">
-            <h2>Moonriver</h2>
-            <GasInfo network={"moonriver"} />
-          </Grid.Column>
-        </Grid>
-        <br />
-        <br />
-        <Grid>
-          <Grid.Column width={8} stretched verticalAlign="top">
-            <h2>Moonbase</h2>
-            <GasInfo network={"moonbase"} />
-          </Grid.Column>
-        </Grid>
-      </Container>
-    );
-  }
-}
+const MoonbeamGasStation = () => {
+  return (
+    <Container>
+      <br />
+      <Head>
+        <title>Moonbeam Gas Station</title>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+      </Head>
+      <Grid>
+        <Grid.Column width={8} stretched verticalAlign="top">
+          <Image src={require("/public/moongas.gif")} width={8} height={1} layout="responsive" />
+        </Grid.Column>
+      </Grid>
+      <h1>Moonbeam Network Basic Gas Station</h1>
+      <Grid>
+        <Grid.Column width={8} stretched verticalAlign="top">
+          <h2>Moonbeam</h2>
+          <GasInfo network={"moonbeam"} />
+        </Grid.Column>
+      </Grid>
+      <Grid>
+        <Grid.Column width={8} stretched verticalAlign="top">
+          <h2>Moonriver</h2>
+          <GasInfo network={"moonriver"} />
+        </Grid.Column>
+      </Grid>
+      <Grid>
+        <Grid.Column width={8} stretched verticalAlign="top">
+          <h2>Moonbase</h2>
+          <GasInfo network={"moonbase"} />
+        </Grid.Column>
+      </Grid>
+      <Grid>
+        <Grid.Column width={8} stretched verticalAlign="top">
+          <h2>Moonbeam Dev Node</h2>
+          <GasInfo network={"moonbeam-dev"} />
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
+};
 
 export default MoonbeamGasStation;
